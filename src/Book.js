bf1d25c05e779b1f0e989c3dd57474003d4e83cb
Russ Nealis <codemunkee@gmail.com>
2017-12-24 07:37:34 -0800

    refactored Book to be a functional component - no state

--- src/Book.js
+++ src/Book.js
@@ -1,42 +1,40 @@
-import React, { Component } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
 
-class Book extends Component {
+const Book = ({book, updateShelf}) => {
 
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    //updateShelf: PropTypes.function.isRequired,
-  };
-
-  onShelfSelect = event => {
+  const onShelfSelect = event => {
     // callsBack to Bookshelf, event.target.value === shelfType
-    this.props.updateShelf(this.props.book.id, event.target.value);
+    updateShelf(book.id, event.target.value);
   };
 
-  render() {
-    const { book } = this.props;
-    return (
-      <div className="book">
-        <div className="book-top">
-          <div className="book-cover" style={{ width: 128,
-                                               height: 193,
-                                               backgroundImage: 'url("' + book.imageLinks.smallThumbnail + '")' }}>
+  return (
+    <div className="book">
+      <div className="book-top">
+        <div className="book-cover"
+             style={{ width: 128,
+                      height: 193,
+                      backgroundImage: 'url("' + book.imageLinks.smallThumbnail + '")' }}>
+        </div>
+          <div className="book-shelf-changer">
+            <select onChange={onShelfSelect}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
           </div>
-            <div className="book-shelf-changer">
-              <select onChange={this.onShelfSelect}>
-                <option value="none" disabled>Move to...</option>
-                <option value="currentlyReading">Currently Reading</option>
-                <option value="wantToRead">Want to Read</option>
-                <option value="read">Read</option>
-                <option value="none">None</option>
-              </select>
-            </div>
-          </div>
-        <div className="book-title">{book.name}</div>
-        <div className="book-authors">{book.authors[0]}</div>
-      </div>
-    )
-  }
-}
+        </div>
+      <div className="book-title">{book.name}</div>
+      <div className="book-authors">{book.authors[0]}</div>
+    </div>
+  )
+};
+
+Book.propTypes = {
+  book: PropTypes.object.isRequired,
+  updateShelf: PropTypes.func.isRequired,
+};
 
 export default Book;
